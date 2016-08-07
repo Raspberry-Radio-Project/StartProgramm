af25e85e72208aa1fcd3ba5c68b1741fc3d21888
Felix <Felix@Felix>
2016-08-07 21:12:16 +0200

    GUI

--- src/com/raspberryradio/touch/TouchGUI.java
+++ src/com/raspberryradio/touch/TouchGUI.java
@@ -7,6 +7,7 @@ package com.raspberryradio.touch;
 
 import java.awt.TextArea;
 import javax.swing.JDialog;
+import javax.swing.UIManager;
 
 /**
  *
@@ -108,11 +109,13 @@ public class TouchGUI extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        /*
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -128,10 +131,16 @@ public class TouchGUI extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(TouchGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(TouchGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+        }*/
         //</editor-fold>
         //</editor-fold>
-
+        
+        try{
+            UIManager.setLookAndFeel(com.sun.java.swing.plaf.gtk.GTKLookAndFeel);
+        }catch(Exception e){
+            
+        }catch()
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
