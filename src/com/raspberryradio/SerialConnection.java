e593cfa1182599b42cc836b05d40fd04e00f23a4
felix <felix@FelixLaptop>
2017-06-25 00:49:55 +0200

    Umbruch

--- src/com/raspberryradio/SerialConnection.java
+++ src/com/raspberryradio/SerialConnection.java
@@ -9,30 +9,34 @@ import jssc.SerialPort;
 import jssc.SerialPortException;
 
 public class SerialConnection {
-    static void connect(){
-        SerialPort serialPort = new SerialPort("/dev/ttyUSB0"); //Dateipfad des SerialChips.
+
+    static void connect() {
+        SerialPort serialPort = new SerialPort("/dev/ttyUSB0");
         try {
             System.out.println("Port opened: " + serialPort.openPort());
-            System.out.println("Params setted: " + serialPort.setParams(9600, 8, 1, 0));
-            System.out.println("\"VS\" successfully writen to port: " + serialPort.writeBytes("VS".getBytes())); //Zeichenkette zum Anschalten des Verstärkers
-            System.out.println("\"VD\" successfully writen to port: " + serialPort.writeBytes("VD".getBytes())); //Zeichenkette zum Durchschalten auf die Lautsprecher
+            System.out.println("Params setted: "
+                    + serialPort.setParams(9600, 8, 1, 0));
+            System.out.println("\"VS\" successfully writen to port: "
+                    + serialPort.writeBytes("VS".getBytes()));
+            System.out.println("\"VD\" successfully writen to port: "
+                    + serialPort.writeBytes("VD".getBytes()));
             System.out.println("Port closed: " + serialPort.closePort());
-        }
-        catch (SerialPortException ex){
-            System.out.println(ex); //Fehlermeldung wird in der Konsole angezeigt.
+        } catch (SerialPortException ex) {
+            System.out.println(ex);
         }
     }
-    
-    static void change(){
-        SerialPort serialPort = new SerialPort("/dev/ttyUSB0"); //Dateipfad des SerialChips.
+
+    static void change() {
+        SerialPort serialPort = new SerialPort("/dev/ttyUSB0");
         try {
             System.out.println("Port opened: " + serialPort.openPort());
-            System.out.println("Params setted: " + serialPort.setParams(9600, 8, 1, 0));
-            System.out.println("\"VI\" successfully writen to port: " + serialPort.writeBytes("VI".getBytes())); //Verstärker wechselt den Audioeingang
+            System.out.println("Params setted: "
+                    + serialPort.setParams(9600, 8, 1, 0));
+            System.out.println("\"VI\" successfully writen to port: "
+                    + serialPort.writeBytes("VI".getBytes()));
             System.out.println("Port closed: " + serialPort.closePort());
-        }
-        catch (SerialPortException ex){
-            System.out.println(ex); //Fehlermeldung wird in der Konsole angezeigt.
+        } catch (SerialPortException ex) {
+            System.out.println(ex);
         }
     }
 }
